507c62262af8d341e72040342e42ce4b869ff1f7
Cenachi <carloscenachi@protonmail.com>
2023-03-02 12:32:23 -0300

    cross-origin

--- src/main/kotlin/com/auth/api/controller/AuthController.kt
+++ src/main/kotlin/com/auth/api/controller/AuthController.kt
@@ -5,12 +5,13 @@ import com.auth.api.dto.RegisterDto
 import com.auth.api.model.User
 import com.auth.api.service.UserService
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-
+@CrossOrigin
 @RestController
 @RequestMapping("api/auth")
 class AuthController(private val service : UserService){
